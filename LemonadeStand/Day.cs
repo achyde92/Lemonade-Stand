09b97d989937877f8e58e3328e9ba5bcad0c7472
Alex Hyde <alexhyde@Alexs-MacBook-Pro.local>
2023-10-27 17:00:21 -0400

    Finish Player Class

--- LemonadeStand/Day.cs
+++ LemonadeStand/Day.cs
@@ -1,10 +1,88 @@
 ﻿using System;
 namespace LemonadeStand
 {
-	public class Day
+	internal class Day
 	{
+		Weather weather;
+		public List<Customer> customers;
+
 		public Day()
 		{
+			customers = new List<Customer>();
+			weather = new Weather();
+		}
+
+		public void StartDay()
+		{
+			FootTraffic();
+			SimulatingCustomersWalkingBy();
+		}
+
+		public void CreateCustomers(int numOfCustomers)
+		{
+			for (int i = 0; i < numOfCustomers; i++)
+			{
+				customers.Add(new Customer());
+			}
+		}
+
+		public int SimulatingCustomersWalkingBy(double lemonadePrice)
+		{
+			int customersWhoBought = 0;
+
+			foreach (Customer customer in customers)
+			{
+				if (customer.WillBuyLemonade(lemonadePrice))
+				{
+					Console.WriteLine("A customer puchased lemonade!");
+					customersWhoBought++;
+				}
+				else
+				{
+					Console.WriteLine("A customer walked away.");
+				}
+			}
+			return customersWhoBought;
+		}
+
+		public int FootTraffic()
+		{
+			int potentialfootTraffic = 0;
+
+			foreach(Customer customer in customers)
+			{
+				if (weather.condition == "Rainy")
+				{
+					Console.WriteLine("The rain is driving away customers");
+					 potentialfootTraffic = potentialfootTraffic + 15;
+				}
+				else if (weather.condition == "Cloudy")
+				{
+					Console.WriteLine("The Weather is mid, expect a moderate number of customers");
+					 potentialfootTraffic = potentialfootTraffic + 25;
+				}
+				else if (weather.condition == "Sunny")
+				{
+					Console.WriteLine("The weather is nice! There should many people out!");
+					potentialfootTraffic = potentialfootTraffic + 35;
+				}
+				if (weather.temperature <=  64)
+				{
+					Console.WriteLine("It's a little chilly today");
+					potentialfootTraffic = potentialfootTraffic - 5;
+				}
+				else if (weather.temperature <= 74 && weather.temperature > 64)
+				{
+					Console.WriteLine("The weather is nearly perfect today");
+					potentialfootTraffic = potentialfootTraffic + 5;
+				}
+				else if (weather.temperature >= 75)
+				{
+					Console.WriteLine("It's hot today. People will be thirsty");
+					potentialfootTraffic = potentialfootTraffic + 10;
+				}
+			}
+			return potentialfootTraffic;
 		}
 	}
 }
--- LemonadeStand/Player.cs
+++ LemonadeStand/Player.cs
@@ -12,16 +12,33 @@ namespace LemonadeStand
         public Inventory inventory;
         public Wallet wallet;
         public Recipe recipe;
+        public string name;
+        public Store store;
 
         // constructor (SPAWNER)
-        public Player()
+        public Player(string name)
         {
             inventory = new Inventory();
             wallet = new Wallet();
             recipe = new Recipe();
+            this.name = name;
+
         }
 
         // member methods (CAN DO)
 
+        public void PlayerName()
+        {
+            Console.WriteLine("What is the players name?");
+            string name = Console.ReadLine();
+        }
+
+        public void PurchaseSupplies()
+        {
+            if ()
+            {
+                Store.SellLemons();
+            }
+        }
     }
 }
